f505af57d560b03397eabee41e652bbdba8ac0c4
Agustin Jaralampidis <ajaralampidis@gmail.com>
2022-02-09 00:45:10 -0300

    feat: version 1.2.0 new section and performance fixes

--- index.js
+++ index.js
@@ -1,61 +1,111 @@
-// ================ NavBar ========================
-const contactButton = document.getElementById('contact');
-const webDevButton = document.getElementById('webDev');
-const artButton = document.getElementById('art');
-const industrialDesignButton = document.getElementById('industrialDesign');
-const multimediaButton = document.getElementById('multimedia');
+const allModals = document.getElementsByClassName("modal")
+const modalOverlay = document.getElementById("modalOverlay")
+const modelViewer = document.getElementsByTagName("model-viewer");
 
-// ================= Modals ========================
-let modalOverlay = document.getElementById('modalOverlay'); // not a modal but the overlay
-let allModals = document.getElementsByClassName('modal') // all modal Html collection (not an array but kind of)
-let contactModal = document.getElementById('contactModal');
-let webDevModal = document.getElementById('webDevModal');
-let artModal = document.getElementById('artModal');
-let industrialDesignModal = document.getElementById('industrialDesignModal');
-let multimediaModal = document.getElementById('multimediaModal');
+
+
+// ================== About Me Typeit ===================
+
+const aboutTypeIt = new TypeIt(".aboutMeContent", {
+  cursorChar: "█",
+  startDelay: 250,
+  waitUntilVisible: true,
+})
+
 
 // ================= Opening Modal EventHandler ===================
 function openModal(event) {
 
   const selector = event.target.id
 
+  let currentModal = document.getElementById(`${selector}Modal`);
+  currentModal.style.visibility = 'visible';
+  currentModal.style.height = '80vh'
   modalOverlay.style.visibility = 'visible';
-
-  switch (selector) {
-    case 'contact':
-      contactModal.style.visibility = 'visible';
-      contactModal.style.height = '80vh'
-      break;
-    case 'webDev':
-      webDevModal.style.visibility = 'visible';
-      webDevModal.style.height = '80vh'
-      break;
-    case 'art':
-      artModal.style.visibility = 'visible';
-      artModal.style.height = '80vh'
-      break;
-    case 'industrialDesign':
-      industrialDesignModal.style.visibility = 'visible';
-      industrialDesignModal.style.height = '80vh'
-      break;
-    case 'multimedia':
-      multimediaModal.style.visibility = 'visible';
-      multimediaModal.style.height = '80vh'
-      break;
-  }
 }
+// ================ NavBar ========================
 
-// ===================== NavBar button eventListeners ===================
+const aboutMeButton = document.getElementById('aboutMe');
+const openModalWrap = (event) => {
+  openModal(event);
+  if(aboutTypeIt.is('frozen')) {
+    aboutTypeIt.unfreeze();  
+  } else {
+    aboutTypeIt
+    .pause(1000)
+    .type("Hello!")
+    .pause(1700)
+    .break()
+    .type("I’m Agustin Jaralampidis. ")
+    .pause(1000)
+    .type("Creative Developer")
+    .pause(800)
+    .type(" ... uhm ...")
+    .pause(1000)
+    .delete(12)
+    .type(" kind of ...")
+    .pause(2000)
+    .delete(12)
+    .move(-18)
+    .type('A ')
+    .move(18)
+    .type(' in progress.')
+    .pause(2000)
+    .break()
+    .break()
+    .type("An <a href='https://www.instagram.com/agustin_jaralampidis/' target='_blank' >Artist</a> who studied <a title='Click to see my Indsustrial Design portfolio' id='industrialDesign' >Industrial Design</a> and works as a <a href='https://wearepsh.com/' target='_blank'>Developer at PSH</a>.")
+    .exec(() => {
+      const industrialDesignButton = document.getElementById('industrialDesign');
+      industrialDesignButton.addEventListener('click', openModal)
+    } )
+    .break()
+    .pause(3500)
+    .break()
+    .type("I would love to <b>Design & Develop</b> for your project!")
+    .break()
+    .type("Please feel free to contact me at <a href='mailto:ajaralampidis@gmail.com'>ajaralampidis@gmail.com</a>.")
+    .break()
+    .pause(2500)
+    .break()
+    .type("Thanks for your time :)")
+    .break()
+    .type("<i>- Agus</i>")
+    .break()
+    .pause(2000)
+    .break()
+    .type("<i>pd: In a mobile device you can see the cube in Augumented Reality, give it a try!</i>")
+    .go();
+  }
 
+}
+aboutMeButton.addEventListener('click', openModalWrap)
+
+const contactButton = document.getElementById('contact');
 contactButton.addEventListener('click', openModal)
+
+const webDevButton = document.getElementById('webDevCV');
 webDevButton.addEventListener('click', openModal)
-artButton.addEventListener('click', openModal)
-industrialDesignButton.addEventListener('click', openModal)
-multimediaButton.addEventListener('click', openModal)
+
+// ================ AR Button =====================
+
+
+const arButton = document.getElementById("arButton");
+
+window.addEventListener("load", () => {
+  
+  console.log("modelViewer.activateAR: ", modelViewer.activateAR)
+
+  if(modelViewer.canActivateAR) {
+    arButton.addEventListener("click", modelViewer.activateAR)
+    arButton.disabled = false;
+  }
+});
+
 
 
 
 // ================== close modals when click on overlay ===================
+
 
 function overlayCloseModal () {
   for (let modal of allModals) {
@@ -63,60 +113,34 @@ function overlayCloseModal () {
     modal.style.visibility = 'hidden';
   }
   modalOverlay.style.visibility = 'hidden'
+
+  if(!aboutTypeIt.is('completed')){
+    aboutTypeIt.freeze()
+  }
+
 }
 modalOverlay.addEventListener('click', overlayCloseModal)
 
-// ================= carousel 
+
+// ====================== Pic Button ============================= //
+/* 
+
+const picButton = document.getElementById("picButton")
+const downloadButton = document.getElementById("downloadButton")
 
 
+async function downloadScreenshot() {
+  // const pic = modelViewer[0].toDataURL().replace("image/png", "image/octet-stream");
+  // window.location.href = pic;
+
+  let idealPic = await modelViewer[0].toBlob({qualityArgument: 0.92, idealAspect: true})
+  // idealPic.replace("image/png", "image/octet-stream")
+  const idealPicurl = URL.createObjectURL(idealPic, {type: 'image/png'})
+  // window.location.href = idealPicurl;
+  downloadButton.href = idealPicurl
+}
 
 
-
-
-
-
-/*
-
-===================== NavBar ==========================
-
-<a class="navElement" id='contact'>
-<span class="navElementIndicator"> > </span> Contact Me !
-</a>
-
-<a class="navElement" id='webDev'>
-<span class="navElementIndicator"> > </span> Web Development
-</a>
-
-<a class="navElement" id='art'>
-<span class="navElementIndicator"> > </span> Art & Illustration
-</a>
-
-<a class="navElement" id='industrialDesign'>
-<span class="navElementIndicator"> > </span> Industrial Design
-</a>
-
-<a class="navElement" id='multimedia'>
-<span class="navElementIndicator"> > </span> Multimedia
-</a>
-
-
-===================== Modals ================================
-
-  <div class='modalOverlay'></div>
-
-  <div id='contactModal' class="modal div"> hola</div>
-
-  <div id='webDevModal' class="modal embed" >
-    <embed src="" type="aplication/pdf" frameborder='0' allowfullscreen>
-  </div>
-
-  <div id='artModal' class="modal div"> </div>
-
-  <div id='industrialDesignModal' class="embed modal">
-    <embed src="./assets/Portfolio-Agustin-Jaralampidis-Small.pdf" type="application/pdf" frameborder="0" allowfullscreen>
-  </div>
-
-  <div id='multimediaModal' class="modal div"> </div>
-
+picButton.addEventListener('click', downloadScreenshot)
 
 */
